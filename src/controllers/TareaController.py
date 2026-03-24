332b20d03a66108a6d5dc26db44cc9cfbe69831c
MiguelPinedaBecerra <233080606010467@cetis61.edu.mx>
2026-03-24 07:39:04 -0500

    ghj

---
+++ src/controllers/TareaController.py
@@ -0,0 +1,16 @@
+from src.models.TareasModel import TareaModel
+
+class TareaController:
+    def __init__(self)
+        self.model = TareaModel()
+        
+    def obtener_lista(self, id_usuario):
+        return self.model.listar_por_usuario(id_usuario)
+    
+    def guardar_nueva(self, id_usuario, titulo, desc, prio, class):
+        if not titulo:
+            return False, "El titulo obligatorio"
+        
+        self.model.crear(id_usuario, titulo, desc, prio, clas)
+        return True, "Tarea guardada"
+    
