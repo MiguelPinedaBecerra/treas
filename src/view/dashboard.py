051e184263814167399dd857d1eddcb66399dd8b
MiguelPinedaBecerra <23308060610467@cetis61.edu.mx>
2026-04-13 11:48:33 -0500

    eqwe

---
+++ src/view/dashboard.py
@@ -0,0 +1,30 @@
+import flet as ft 
+
+def DashboardView(page, tarea_controller):
+    user = page.session.get("user")
+    lista_tareas = ft.Column(scroll=ft.ScrollMode.ALWAYS, expand=True)
+    
+    def refresh():
+        lista_tareas.controls.clear()
+        for t in tarea_controller.obtener_lista(user['id_usuario']):
+            lista_tareas.controls.append(
+                ft.Card(
+                    content=ft.Container(
+                        content=ft.ListTile(
+                            title=ft.Text(t['titulo'], weight="bold"),
+                            subtitle=ft.Text(f"{t['description']}\nPrioridad: {t['prioridad']}"),
+                            trailing=ft.Badge(content=ft.Text(t['estadp']), bgcolor=ft.Colors.ORANGE_300)
+                        ), padding=30
+                    )
+                )
+            )
+            page.update()
+            
+            
+    txt_titulo = ft.TextField(label="Nueva Tarea", expand=True)
+    
+    def add_task(e):
+        success, msg = tarea_cotroller.guardar_nueva(user['id_usuario'], txt_titulo.value, "", "media", "trabajo")
+        if success:
+            txt_titulo.value = ""
+            refresh()
--- src/view/loginView.py
+++ src/view/loginView.py
@@ -1,8 +1,8 @@
 import flet as ft
 
-def LoginView(page, auth_controller):
-    email_input = ft.TextField(label="Correo electronico", with=350, border_radius=10)
-    pass_imput = ft.TextField(label="Contraseña", password=True, with=350, border_radius=10)
+def loginView(page, auth_controller):
+    email_input = ft.TextField(label="Correo electronico", width=350, border_radius=10)
+    pass_imput = ft.TextField(label="Contraseña", password=True, width=350, border_radius=10)
     
     def login_click(e):
         user, msg = auth_controller.login(email_input.value, pass_imput.value)
@@ -18,12 +18,10 @@ def LoginView(page, auth_controller):
             ft.AppBar(tittle=ft.Text("SIGE - login"), bgcolor=ft.Colors.BLUE_GREY_900, color="white"),
             ft.Colunm([
                 ft.Icon(ft.Icons.LOCK_PERSON, size=50, color=ft.Colors.BLUE),
-            ])
-            
-            
-            
-            
-            
-            
-            
+                ft.Text("Acceso al sistema", size=24, weigth="bold"),
+                email_input,
+                pass_input,
+                ft.ElevateButton("Entrar", on_click=login_click, width=350),
+                ft.TextButton("Crear una cuenta nueva", on_click=lambda _: page.go("/registro"))
+            ], horizontal_alignment0ft.CrossAxisAlignment.CENTER, alignment=ft.NainAxisAlignment.CENTER)
         ])
