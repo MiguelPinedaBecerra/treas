8f59152f2a5a4318039d13fc3f2c1c66af371c05
MiguelPinedaBecerra <23308060610467@cetis61.edu.mx>
2026-04-22 11:36:20 -0500

    olayosoyese

--- src/main.py
+++ src/main.py
@@ -1,43 +1,55 @@
 import flet as ft
 from controllers.UserController import AuthController
 from controllers.TareaController import TareaController
-from views.loginView import LoginView
+from views.LoginView import LoginView
 from views.dashboard import DashboardView
 
 def start(page: ft.Page):
-    #Configuracion inicial de la pagina
-    page.tile = "Sistema SIGE"
+    page.title="Sistema de inicio de sesion"
     page.window_width = 450
     page.window_height = 700
     
     auth_ctrl = AuthController()
     task_ctrl = TareaController()
     
-    def route_change(route):
-        
+    def route_change(e):
         page.views.clear()
+        
+        # caso 1: login
         if page.route == "/":
-            
             page.views.append(LoginView(page, task_ctrl))
+            
         elif page.route == "/dashboard":
             page.views.append(DashboardView(page, task_ctrl))
-            
+        
+        #caso de seguridad    
         if not page.views:
             page.views.append(
-                ft.view("/", [ft.Text("Error: Ruta no encontrada o vista vacia")])
+                ft.View("/", [ft.Text("Error: Ruta no encontrada o vista vacia")])
             )
+            
         page.update()
         
-    def view_pop(e):
-            if len(page.views) > 1:
-                page.views.pop()
-                top_view = page.view[-1]
-                page.go(top_view.route)
         
+    def view_pop(e):
+        if len(page.views) > 1:
+            page.views.pop()
+            top_view = page.view[-1]
+            page.go(top_view.route)
+    
+    #1. asignar los manejadores de evento primero
     page.on_route_change = route_change
     page.on_view_pop = view_pop
     
+    #2. IMPORTANTE, no fuerces page.route = ""
+    print("Iniciando navegacion....")
+    if page.route=="/":
+        route_change(None)
+    else:
+        page.go("/")
+
 def main():
+    #ejecucion de la app
     ft.app(target=start)
     
 if __name__ == "__main__":
--- src/views/loginView.py
+++ src/views/loginView.py
@@ -1,27 +1,49 @@
 import flet as ft
 
-def LoginView(page, auth_controller):
-    email_input = ft.TextField(label="Correo electronico", width=350, border_radius=10)
-    pass_imput = ft.TextField(label="Contraseña", password=True, width=350, border_radius=10)
+def LoginView(page: ft.Page, auth_controller):
+    email_input = ft.TextField(label="Correo Electronico", width = 350, border_radius=10)
+    pass_input = ft.TextField(label="Contraseña", password=True, can_reveal_password=True, width=350, border_radius=10)
     
     def login_click(e):
-        user, msg = auth_controller.login(email_input.value, pass_imput.value)
+        if not email_input.value or not pass_input.value:
+            page.snack_bar = ft.Snack_bar = ft.SnackBar(ft.Text("Por favor, llene todos los campos"))
+            page.snack_bar.open = True
+            page.update()
+            return
+
+        user, msg = auth_controller.login(email_input.value, pass_input.value)
+        
         if user:
             page.session.set("user", user)
             page.go("/dashboard")
-        else:
-            page.snack.bar = ft.SnackBar(ft.Text(msg))
+        else: 
+            page.snack_bar = ft.SnackBar(ft.Text(msg))
             page.snack_bar.open = True
             page.update()
             
-        return ft.View("/", [
-            ft.AppBar(tittle=ft.Text("SIGE - login"), bgcolor=ft.colors.BLUE_GREY_900, color="white"),
-            ft.Colunm([
-                ft.Icon(ft.Icons.LOCK_PERSON, size=50, color=ft.colors.BLUE),
-                ft.Text("Acceso al sistema", size=24, weigth="bold"),
-                email_input,
-                pass_input,
-                ft.ElevateButton("Entrar", on_click=login_click, width=350),
-                ft.TextButton("Crear una cuenta nueva", on_click=lambda _: page.go("/registro"))
-            ], horizontal_alignment=ft.CrossAxisAlignment.CENTER, alignment=ft.NMainAxisAlignment.CENTER)
-        ])
+    login_button = ft.ElevatedButton("Entrar", on_click=login_click, width=350, bgcolor="blue", color = "white")
+    registrar = ft.ElevatedButton("Crear una nueva cuenta", bgcolor="green", color = "white", on_click=lambda _: page.go("/registro"))
+    pass_input.on_submit = login_click
+        
+    
+    return ft.View(
+        route = "/",
+        vertical_alignment=ft.MainAxisAlignment.CENTER,
+        horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+        appbar=ft.AppBar(title=ft.Text("SIGE - login"), bgcolor="bluegrey900", color= "white"),
+        controls=[
+            ft.Column(
+                [
+                    ft.Text("Acceso al sistema", size=24, weight="bold"),
+                    email_input,
+                    pass_input,
+                    login_button,
+                    registrar,
+                    ft.TextButton("¿Olvidaste la contraseña?", on_click=lambda _: page.go("/registro"))
+                ],
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                tight=True,
+                spacing=20
+            )
+        ]
+    )
