20d3070d958ba8805d602aa3634c50dd44c62b51
MiguelPinedaBecerra <23308060610467@cetis61.edu.mx>
2026-04-29 11:52:34 -0500

    ola123

--- src/models/UserModel.py
+++ src/models/UserModel.py
@@ -35,3 +35,38 @@ class UsuarioModel:
         if user and bycript.checkpw(password.encode('utf-8'), user['password'].encode('utf-8')):
             return user
         return None
+    
+    def iniciar_sesion(self, usuario_data):
+        conn = None
+        cursor = None
+        try: 
+            #1. establecer conexion
+            conn = self.db.get_connection()
+            cursor = conn.cursor+(dictionary=True) #dictionary facilita leer por nombre de columna
+            
+            #2. Definir la consilta
+            #IMPORTANTE: Usamos placeholders (%) para evitar la Inyeccion SQL
+            query = "SELECT = FROM usuarios WHERE email = %s"
+            values = (usuarios_data.email,)
+            
+            #3.Ejecutar y obtener resultado
+            cursor.execute(query, values)
+            usuario_encontrado = cursor.fetchone()
+            
+            #4. Logica de rentorno
+            if usuario_encontrado:
+                #Verificar la contraseña usando bycript
+                if bycript.checkpw(usuario_data.contrseña.encode('utf-8'), usuario_encontrado['contraseña'].encode('utf-8')):
+                    return True #Credenciales validas
+                else:
+                    return False #No se encontro el usuario o la clave no coincidio
+            else:
+                return False #No se encontro el usuario o la clave no coincide
+            
+        except Expedition as err:
+            print(f"Error en la base de datos: (err)")
+            return False
+        finally:
+            #5. Siempre crrar cursor y connexion
+            if cursor: cursor.close()
+            if conn.close()
